e1adc035d66927dfa0402c7b376f09acb298949a
covers1624 <laughlan.cov@internode.on.net>
2021-06-13 06:33:16 +0930

    Strip old utilities, these are available from Quack now.

--- src/main/groovy/net/covers1624/gradlestuff/util/ThrowingProducer.java
+++
@@ -1,9 +0,0 @@
-package net.covers1624.gradlestuff.util;
-
-/**
- * Created by covers1624 on 11/02/19.
- */
-public interface ThrowingProducer<T, E extends Throwable> {
-
-    T get() throws E;
-}
--- src/main/groovy/net/covers1624/gradlestuff/util/ThrowingRunnable.java
+++
@@ -1,9 +0,0 @@
-package net.covers1624.gradlestuff.util;
-
-/**
- * Created by covers1624 on 11/02/19.
- */
-public interface ThrowingRunnable<E extends Throwable> {
-
-    void run() throws E;
-}
